a366d7fa610fe0d31154a64c280d9135c0658a9d
KuroSys <102257683+KuroSys@users.noreply.github.com>
2023-08-04 21:36:18 +0200

    Update pong.py
    Code is not async & does not ratelimits anymore

--- pong.py
+++ pong.py
@@ -1,14 +1,16 @@
 # non local imports ^_^
 # if your reading this use auto-py-to-exe to convert this to an .exe file
-import socket
+from colorama import Fore, Back, Style
+import asyncio
 import time
 import sys
+import socket
 import requests
-from colorama import Fore, Back, Style
 
-TIMEOUT = 3
+x = 0
+responses = []
 
-def get_isp_from_api(ip):
+async def get_isp_from_api(ip):
     url = f"http://ip-api.com/json/{ip}"
     response = requests.get(url)
 
@@ -18,41 +20,29 @@ def get_isp_from_api(ip):
     else:
         print(f"Failed to fetch ISP information. Status code: {response.status_code}")
         return None
+    pass
 
-def get_asn_from_api(asn):
-    url = f"http://ip-api.com/json/{ip}"
-    response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        return data["as"]
-    else:
-        print(f"Failed to fetch ASN. Status code: {response.status_code}")
-        return None
-
-def print_help():
-    print("Usage: pong.exe host port")
-    print("Optional:")
-    print("  -as         Shows ASN Informations (22 REQ Limit per IP)")
-    print("  -h, --help  Show this help message and exit.")
-    sys.exit(0)
-
-def measure_tcp_response_time(host, port, timeout):
+async def ping(host, port, x):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    timeout = 1
     try:
         client_socket.settimeout(timeout)
 
-        time.sleep(1)
+        await asyncio.sleep(1)
 
         start_time = time.time()
         client_socket.connect((host, port))
         end_time = time.time()
 
         response_time = (end_time - start_time) * 1000
+        responses.insert(x,
+                         f"time= {Fore.GREEN}{response_time:.2f}ms{Fore.RESET} - port={Fore.GREEN}{port}{Fore.RESET} ")
         return response_time
 
     except socket.timeout:
+        responses.insert(x,
+                         f"{Fore.RED}TIMED OUT{Fore.RESET} - port={Fore.RED}{port}{Fore.RESET} ")
         return None
     except socket.error as e:
         print(f"Error: {e}")
@@ -60,34 +50,24 @@ def measure_tcp_response_time(host, port, timeout):
     finally:
         client_socket.close()
 
-if __name__ == "__main__":
-    if len(sys.argv) == 2 and (sys.argv[1] == "-h" or sys.argv[1] == "--help"):
-        print_help()
 
-    if "-as" in sys.argv:
-        show_asn = True
-        sys.argv.remove("-as")
-    else:
-        show_asn = False
+async def startup():
 
-    if len(sys.argv) != 3:
-        print("Error: Invalid number of arguments.")
-        print_help()
-
+    global x
     host = sys.argv[1]
-    port = int(sys.argv[2])
-
+    ports = [int(c) for c in sys.argv[2].split(',')]
+    ip = socket.gethostbyname(host)
+    isp = await get_isp_from_api(ip)
     while True:
-        response_time = measure_tcp_response_time(host, port, TIMEOUT)
+        for port in ports:
+            x = x + 1
+            asyncio.create_task(ping(host, port, x))
+        all_tasks = asyncio.all_tasks()
+        current_task = asyncio.current_task()
+        all_tasks.remove(current_task)
+        await asyncio.wait(all_tasks)
+        print(f"Connected to {Fore.GREEN}{host}{Fore.RESET} ({Fore.MAGENTA}{isp}{Fore.RESET}) {''.join(responses)} ")
+        responses.clear()
+        x = 0
 
-        ip = socket.gethostbyname(host)
-        isp = get_isp_from_api(ip)
-        asn = get_asn_from_api(ip)
-
-        if response_time is not None:
-            if show_asn:
-                print(f"Connected to {Fore.GREEN}{host}{Fore.RESET} ({Fore.MAGENTA}{asn}{Fore.RESET}) - time= {Fore.GREEN}{response_time:.2f}ms{Fore.RESET} - port={Fore.GREEN}{port}{Fore.RESET} - protocol={Fore.GREEN}TCP{Fore.RESET}")
-            else:
-                print(f"Connected to {Fore.GREEN}{host}{Fore.RESET} ({Fore.MAGENTA}{isp}{Fore.RESET}) - time= {Fore.GREEN}{response_time:.2f}ms{Fore.RESET} - port={Fore.GREEN}{port}{Fore.RESET} - protocol={Fore.GREEN}TCP{Fore.RESET}")
-        else:
-            print(f"{Fore.LIGHTRED_EX}Connection timed out{Fore.RESET}")
+asyncio.run(startup())
